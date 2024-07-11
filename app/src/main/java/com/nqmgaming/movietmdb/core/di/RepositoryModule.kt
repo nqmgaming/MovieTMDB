9fc5bfc19b11c5a5cbdf16bd4f5952c8bdc90478
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 13:57:13 +0700

    feat: Provide MovieTmdbRepository
    Refactored RepositoryModule to provide MovieTmdbRepository using Hilt's @Provides annotation.
    The repository
     implementation now depends on TMDBApi.

--- app/src/main/java/com/nqmgaming/movietmdb/core/di/RepositoryModule.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/core/di/RepositoryModule.kt
@@ -1,16 +1,17 @@
 package com.nqmgaming.movietmdb.core.di
 
+import com.nqmgaming.movietmdb.core.data.network.TMDBApi
+import com.nqmgaming.movietmdb.core.repository.MovieTmdbRepositoryImpl
 import com.nqmgaming.movietmdb.domain.repository.MovieTmdbRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-@InstallIn(SingletonComponent::class)
 @Module
-internal abstract class RepositoryModule {
-    @Binds
-    abstract fun bindMovieRepository(
-        movieTmdbRepository: MovieTmdbRepository
-    ): MovieTmdbRepository
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    fun provideMovieRepository(api: TMDBApi): MovieTmdbRepository = MovieTmdbRepositoryImpl(api)
 }
