a7a0aeb41e7925626c9cb91af8bab2908d55ff3d
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 13:57:40 +0700

    feat: Add Search BottomNavItem
    Added a new BottomNavItem for the Search screen, including its title, icon, and route.

--- app/src/main/java/com/nqmgaming/movietmdb/presentation/BottomNavItem.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/presentation/BottomNavItem.kt
@@ -2,6 +2,7 @@ package com.nqmgaming.movietmdb.presentation
 
 import com.nqmgaming.movietmdb.R
 import com.ramcosta.composedestinations.generated.destinations.HomeScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.SearchScreenDestination
 
 sealed class BottomNavItem(
     val title: String,
@@ -12,5 +13,10 @@ sealed class BottomNavItem(
         title = "Home",
         icon = R.drawable.ic_home,
         route = HomeScreenDestination.route
+    )
+    data object Search : BottomNavItem(
+        title = "Search",
+        icon = R.drawable.ic_search,
+        route = SearchScreenDestination.route
     )
 }
