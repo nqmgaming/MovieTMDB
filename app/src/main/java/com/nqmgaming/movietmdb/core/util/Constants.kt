3ee53971c85ffa25a3a266bf8140cedc35f5aa11
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 12:45:33 +0700

    feat: Add image base URL and paging size constants
    Added constants for the image base URL and paging size to be used in the application.

--- app/src/main/java/com/nqmgaming/movietmdb/core/util/Constants.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/core/util/Constants.kt
@@ -2,4 +2,6 @@ package com.nqmgaming.movietmdb.core.util
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+    const val PAGING_SIZE = 20
 }
