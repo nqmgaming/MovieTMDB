14b0fe70f287d04d0935b29296567094cc72a794
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 11:49:47 +0700

    feat: Setup network module
    Added dependencies for Retrofit, OkHttp, and Gson.
    Created a network module to provide TMDBApi instance using Retrofit.
    
    Included logging interceptor for debugging network requests.

--- app/src/main/java/com/nqmgaming/movietmdb/core/di/AppModule.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/core/di/AppModule.kt
@@ -1,4 +1,53 @@
 package com.nqmgaming.movietmdb.core.di
 
+import com.nqmgaming.movietmdb.core.data.network.TMDBApi
+import com.nqmgaming.movietmdb.core.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    // Provide the logging interceptor
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    // Provide the OkHttp client
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+
+        return okHttpClient.build()
+    }
+
+    // Provide the TMDB API
+    @Provides
+    @Singleton
+    fun providesTMDBApi(okHttpClient: OkHttpClient): TMDBApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(TMDBApi::class.java)
+    }
+
 }
