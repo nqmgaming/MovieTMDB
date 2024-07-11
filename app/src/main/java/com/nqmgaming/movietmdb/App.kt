534238de6c6e435500b51c423b63ea5da02e2064
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 11:49:39 +0700

    feat: Initialize Timber for logging
    Added Timber dependency and initialized it for debug builds to enable logging.

--- app/src/main/java/com/nqmgaming/movietmdb/App.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/App.kt
@@ -1,5 +1,16 @@
 package com.nqmgaming.movietmdb
 
 import android.app.Application
+import timber.log.Timber
 
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Timber initialization
+       if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+    }
+}
