80cef107441c32d2d81e6040f54369bc6d50c3d9
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-11 13:58:10 +0700

    feat: Implement StandardScaffold and integrate Compose Destinations
    Created a StandardScaffold composable to provide a consistent layout with a bottom navigation bar.
    Integrated Compose Destinations for
     type-safe navigation and added navigation logic to the scaffold.
    Updated MainActivity to use the new scaffold and Compose Destinations NavHost.

--- app/src/main/java/com/nqmgaming/movietmdb/MainActivity.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,12 +16,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.nqmgaming.movietmdb.presentation.StandardScaffold
 import com.nqmgaming.movietmdb.ui.theme.MovieTMDBTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.generated.NavGraphs
+import com.ramcosta.composedestinations.generated.destinations.HomeScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.SearchScreenDestination
+import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalSharedTransitionApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,8 +39,25 @@ class MainActivity : ComponentActivity() {
 
                 val newBackStackEntry by navController.currentBackStackEntryAsState()
                 val route = newBackStackEntry?.destination?.route
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                StandardScaffold(
+                    navController = navController,
+                    showBottomBar = route in listOf(
+                        HomeScreenDestination.route,
+                        SearchScreenDestination.route
+                    )
+                ) { innerPadding ->
+                    SharedTransitionLayout {
+                        DestinationsNavHost(
+                            modifier = Modifier.padding(innerPadding),
+                            navGraph = NavGraphs.root,
+                            navController = navController,
+                            engine = navHostEngine,
+                            dependenciesContainerBuilder = {
+                                dependency(this@SharedTransitionLayout)
+                            }
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/nqmgaming/movietmdb/presentation/StandardScaffold.kt
+++ app/src/main/java/com/nqmgaming/movietmdb/presentation/StandardScaffold.kt
@@ -1,16 +1,78 @@
 package com.nqmgaming.movietmdb.presentation
 
-import android.content.ClipData.Item
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun StandardScaffold(
     navController: NavController,
     showBottomBar: Boolean = true,
     items: List<BottomNavItem> = listOf(
-        BottomNavItem.Home
-    )
+        BottomNavItem.Home,
+        BottomNavItem.Search,
+    ),
+    content: @Composable (paddingValues: PaddingValues) -> Unit,
 ) {
+
+    Scaffold(
+        bottomBar = {
+            if (showBottomBar) {
+                NavigationBar(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onBackground,
+                    content = {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+                        items.forEach { item ->
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        modifier = Modifier.size(24.dp),
+                                        painter = painterResource(id = item.icon),
+                                        contentDescription = item.title,
+                                    )
+                                },
+                                label = {
+                                    Text(
+                                        text = item.title,
+                                        style = MaterialTheme.typography.bodySmall,
+
+                                    )
+                                },
+                                alwaysShowLabel = true,
+                                selected = currentDestination?.route?.contains(item.route) == true,
+                                onClick = {
+                                    navController.navigate(item.route){
+                                        navController.graph.startDestinationRoute?.let { screenRoute ->
+                                            popUpTo(screenRoute) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    ) { paddingValues ->
+        content(paddingValues)
+    }
 
 }
